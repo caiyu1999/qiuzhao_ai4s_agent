2d998e5b66c0edffdc949a2251c0f40030e574d5
Cai Yu <1060912099@qq.com>
2025-08-09 22:00:30 +0800

    用于展示的版本

--- openevolve_graph/Config/__init__.py
+++ openevolve_graph/Config/__init__.py
@@ -1,10 +1,29 @@
+# openevolve_graph/Config/__init__.py
+
 from .graph_config import GraphConfig 
 from .island_config import IslandConfig 
-from .llm_config import ModelConfig,LLMConfig
+from .llm_config import ModelConfig, LLMConfig
 from .prompt_config import PromptConfig
 from .evaluator_config import EvaluatorConfig
 from .controller_config import ControllerConfig
-from .config import Config 
+from .config import Config
+from .rag_config import RAGConfig  # 新增RAG配置导入
 
+# 类型别名定义
+ConfigTypes = (
+    Config | GraphConfig | IslandConfig | 
+    ModelConfig | LLMConfig | PromptConfig |
+    EvaluatorConfig | ControllerConfig | RAGConfig
+)
 
-__all__ = ["Config","GraphConfig","ModelConfig","LLMConfig","PromptConfig","EvaluatorConfig","ControllerConfig"]
+__all__ = [
+    "Config",
+    "GraphConfig",
+    "ModelConfig",
+    "LLMConfig",
+    "PromptConfig",
+    "EvaluatorConfig",
+    "ControllerConfig",
+    "RAGConfig",  # 暴露RAG配置
+    "ConfigTypes"  # 暴露类型别名
+]
--- test.py
+++
@@ -1,4 +0,0 @@
-from langchain.utils.strings import collapse_whitespace
-
-raw_text = "物 拟 人 算 法 设 计 ． R e s e a r c h B a c k g r o u n d"
-cleaned = collapse_whitespace(raw_text)  # 基础清理
